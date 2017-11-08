bdb4531466b0d3d756b6bb4b08eff0bfd4730214
Serhii Polokhalo <kitsonkit@gmail.com>
2017-11-08 07:12:19 -0500

    play with DOM

--- build.sbt
+++ build.sbt
@@ -1,7 +1,11 @@
 enablePlugins(ScalaJSPlugin)
 
 name := "OTable"
-scalaVersion := "2.12.2" // or any other Scala version >= 2.10.2
+scalaVersion := "2.12.3" // or any other Scala version >= 2.10.2
 
 // This is an application with a main method
 scalaJSUseMainModuleInitializer := true
+
+// https://mvnrepository.com/artifact/org.scala-js/scalajs-dom_sjs0.6_2.12
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.9.3"
+
--- src/main/scala/com/kitsonkit/otable/OTable.scala
+++ src/main/scala/com/kitsonkit/otable/OTable.scala
@@ -1,10 +1,29 @@
 package com.kitsonkit.otable
 
-class OTable {
 
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExportTopLevel
+
+class OTable {
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    val parNode = document.createElement("p")
+    val textNode = document.createTextNode(text)
+    parNode.appendChild(textNode)
+    targetNode.appendChild(parNode)
+  }
 }
+
 object OTable {
+  val c = new OTable
   def main(args: Array[String]): Unit = {
+    c.appendPar(document.body,"Hello World!")
     println("Hello world!")
+  }
+
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    c.appendPar(document.body, "You clicked the button!")
   }
 }
